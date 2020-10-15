cd3d2118a456a257202d8339a5941150b6af98f4
Hayeong Jeong <hy78733@gmail.com>
2020-10-15 21:18:29 +0900

    Update crawling-name.py

--- crawling-name.py
+++ crawling-name.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
 import time
-
-# 드라이버 경로 지정 (크롬 이용) -> 컴퓨터에 chromedriver 설치해야 함 (구글에 치면 다운 가능)
 from selenium.common.exceptions import NoSuchElementException
 
+##### 지정해야 할 변수들 #####
+# 연예인 이름 리스트 파일
 output_file = open('/Users/hayeong/DataScience/project/namelist_male_actor.txt', 'w')
+# 드라이버 경로 지정 (크롬 이용) -> 컴퓨터에 chromedriver 설치해야 함 (구글에 치면 다운 가능)
 driver = webdriver.Chrome('/Users/hayeong/chromedriver')
+# 크롤링 할 url: 위키피디아 '분류:대한민국의 남자/여자 배우/가수'
+url = f'https://ko.wikipedia.org/wiki/%EB%B6%84%EB%A5%98:%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%EB%82%A8%EC%9E%90_%EB%B0%B0%EC%9A%B0'
+
+
+# 다음 페이지가 없을 때 True
 isFinished = False
+# 크롤링한 이름의 개
 count = 0
 
 
@@ -27,7 +34,6 @@ def click_next_page():
 
 
 print("ㅡ 크롤링 시작 ㅡ")
-url = f'https://ko.wikipedia.org/wiki/%EB%B6%84%EB%A5%98:%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%EB%82%A8%EC%9E%90_%EB%B0%B0%EC%9A%B0'
 driver.get(url)
 driver.maximize_window()
 
